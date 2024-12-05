8c69c57c3dfb9662cd1000c0d6e0ec1382809bfc
alibougarne <bougarne.ali@gmail.com>
2024-12-05 13:28:19 +0100

    - add speed insights from vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import SplitInterval from "./features/splitInterval/SplitInterval";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
     return (
         <div className="app_wrapper">
+            <SpeedInsights/>
             <SplitInterval/>
         </div>
     );
