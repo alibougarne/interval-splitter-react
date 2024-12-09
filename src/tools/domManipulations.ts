ed5fa4f14bcf0cb0a96bc3408ce448d33d7c24ed
alibougarne <bougarne.ali@gmail.com>
2024-12-09 14:28:22 +0100

    - fix dom manipulation for magic ranges

--- src/tools/domManipulations.ts
+++ src/tools/domManipulations.ts
@@ -6,16 +6,18 @@ export const regroupEmptyPeriods = (periods: Interval[], divCollector: string) =
         const startSelector = document.querySelector(`.range-${periods[0].start}_${periods[0].end}`) as HTMLElement;
         const endSelector = document.querySelector(`.range-${periods[periods.length - 1].start}_${periods[periods.length - 1].end}`) as HTMLElement;
 
-        const startSelectorPosition = startSelector.getBoundingClientRect().x;
-        const startSelectorWidth = startSelector.getBoundingClientRect().width;
+        const startSelectorPosition = parseInt(startSelector.getBoundingClientRect().x.toFixed(0));
+        const startSelectorWidth = parseInt(startSelector.getBoundingClientRect().width.toFixed(0));
 
-        const endSelectorPosition = endSelector.getBoundingClientRect().x;
-        const endSelectorWidth = endSelector.getBoundingClientRect().width;
+        const endSelectorPosition = parseInt(endSelector.getBoundingClientRect().x.toFixed(0));
+        const endSelectorWidth = parseInt(endSelector.getBoundingClientRect().width.toFixed(0));
 
         if (equal_ranges_collector) {
             equal_ranges_collector.style.display = "block";
-            equal_ranges_collector.style.width = `${endSelectorPosition! - startSelectorPosition!}px`;
-            equal_ranges_collector.style.left = `${(startSelector.offsetLeft!) + ((endSelectorWidth + startSelectorWidth) / 4)}px`;
+            const width = endSelectorPosition + parseInt((endSelectorWidth/2).toFixed(0)) - startSelectorPosition - parseInt((startSelectorWidth/2).toFixed(0));
+            const left = ((startSelector.offsetLeft!) + parseInt(((startSelectorWidth) / 2).toFixed(0)))
+            equal_ranges_collector.style.width = `${width-2}px`;
+            equal_ranges_collector.style.left = `${left}px`;
         }
     } else {
         equal_ranges_collector.style.display = "none";
